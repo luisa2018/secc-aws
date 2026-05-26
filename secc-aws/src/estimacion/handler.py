430fb2ca2b71bc6c9b1fefef3cea77c2767c576a
Luisa <35573616+luisa2018@users.noreply.github.com>
2026-05-26 09:03:10 -0500

    fix cors

--- secc-aws/src/estimacion/handler.py
+++ secc-aws/src/estimacion/handler.py
@@ -126,8 +126,7 @@ def response(status_code, body):
     return {
         "statusCode": status_code,
         "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*"
+            "Content-Type": "application/json"
         },
         "body": json.dumps(body)
     }
